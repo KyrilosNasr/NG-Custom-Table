875fb700d9f5e832dd50e232ef26a962703ba6da
Kerelos <kyrilosnasreissa@gmail.com>
2024-05-19 21:17:13 +0300

    [REMOVE]:removed unwanted component

--- src/app/table/table.module.ts
+++ src/app/table/table.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CustomTableComponent } from './components/custom-table/custom-table.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SmartFormatPipe } from './pipes/SmartFormat.pipe';
-import { DragDropAreaComponent } from './components/drag-drop-area/drag-drop-area.component';
 
 
 @NgModule({
   declarations: [
     CustomTableComponent,
     SmartFormatPipe,
-    DragDropAreaComponent
   ],
   imports: [
     CommonModule,

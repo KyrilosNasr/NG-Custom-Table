2ef50218ea983dd75dc8a550b5c2ec9fb3a17efc
Kerelos <kyrilosnasreissa@gmail.com>
2024-05-28 14:44:28 +0300

    [ENHANCE]: enhanced table actions to be 1 list and dynamicly display allowed actions per row

---
+++ src/app/table/utilities/action.constants.ts
@@ -0,0 +1,12 @@
+
+
+
+  const stateActionMapping: { [stateId: number]: string[] } = {
+    1: ['view', 'accept', 'reject'],
+    2: ['view', 'delete'],
+    3:['view']
+  };
+
+
+
+  export {stateActionMapping };
